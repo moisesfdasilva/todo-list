e2f1975a3479b412250b91cef78fe6bbd334cfec
moisesfdasilva <msesfernandes@gmail.com>
2022-07-12 09:39:44 -0300

    Realizado o requisito 7, em script.js.

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+function corDeFundo(evento) {
+  const ponto = evento.target;
+  ponto.className = 'cor-de-fundo';
+}
+
 function adicionar() {
   const elementoNovo = document.createElement('li');
   const elementoDaLista = document.getElementById('texto-tarefa').value;
   elementoNovo.innerText = elementoDaLista;
   document.getElementById('lista-tarefas').appendChild(elementoNovo);
-  console.log(document.querySelectorAll('#lista-tarefas li'));
   document.getElementById('texto-tarefa').value = '';
+  const contador = document.querySelectorAll('#lista-tarefas li').length;
+  for (let elemento = 0; elemento < contador; elemento += 1) {
+    document.getElementsByTagName('li')[elemento].addEventListener('click', corDeFundo);
+  }
 }
 
 document.getElementById('criar-tarefa').addEventListener('click', adicionar);
