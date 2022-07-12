c1cc122e42893043d4cba3c129ceb02f719ca1f8
moisesfdasilva <msesfernandes@gmail.com>
2022-07-12 00:21:00 -0300

    Realizados o requisito 4, em index.html.

---
+++ script.js
@@ -0,0 +1,7 @@
+// function adicionar() {
+//   const elementoDaLista = document.getElementById('texto-tarefa').innerText;
+//   document.getElementById('texto-tarefa').appendChild(document.createElement('li'));
+//   document.querySelectorAll('#texto-tarefa li')[0].innerText = elementoDaLista;
+// }
+
+// document.getElementById('criar-tarefa').addEventListener('click', adicionar);
